6b144c04519976b6f6823d5b91a20a987d9a0e22
demersonrodrigues <demersona.rodrigues@gmail.com>
2024-02-29 23:57:04 -0300

    refactor: Connection server adjustment

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const fastify = require('fastify')({ logger:true });
 const routes = require('./router');
 
+
 fastify.register(routes);
 
 fastify.listen(3000, (err, adress) => {
--- api/router.js
+++ api/router.js
@@ -1,21 +1,64 @@
 // router.js
 const fastifyPlugin = require('fastify-plugin');
+const connection = require('./src/models/connection');
+
+console.log(connection);
 
 async function routes(fastify, options) {
+
+  const conn = await connection;
+
+  fastify.route({
+    method: 'GET',
+    url: '/',
+    handler: async (request, reply) => {
+      return true;
+    }
+  });
+
   // Rotas para usuários
   fastify.route({
     method: 'GET',
     url: '/users',
-    handler: async (request, reply) => {
-      // Implemente a lógica para listar usuários
+    handler: async () => {
+    
+      try {
+        const [results] = await conn.query(
+          'SELECT * FROM `users`'
+        );
+        
+        // console.log(results);
+        return results;
+      } catch (err) {
+        console.log(err);
+      }
     }
   });
+  
+  fastify.route({
+    method: 'GET',
+    url: '/users/:id',
+    handler: async (request) => {
+      try {
+        const userId = request.params.id;
+        const [results] = await conn.query(
+          `SELECT * FROM users WHERE id_user = ${userId}`
+        );
+        
+        // console.log(results);
+        return results;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+});
+
 
   fastify.route({
     method: 'POST',
     url: '/users',
     handler: async (request, reply) => {
-      // Implemente a lógica para criar um usuário
+      
     }
   });
 
@@ -24,7 +67,7 @@ async function routes(fastify, options) {
     method: 'GET',
     url: '/vehicles',
     handler: async (request, reply) => {
-      // Implemente a lógica para listar veículos
+    
     }
   });
 
@@ -32,7 +75,7 @@ async function routes(fastify, options) {
     method: 'POST',
     url: '/vehicles',
     handler: async (request, reply) => {
-      // Implemente a lógica para criar um veículo
+      
     }
   });
 }
