c325044a603ed50100921ed7da3e7972757ae12e
demersonrodrigues <demersona.rodrigues@gmail.com>
2024-02-29 23:57:04 -0300

    refactor: Connection server adjustment #API

--- api/src/models/connection.js
+++ api/src/models/connection.js
@@ -1,9 +1,24 @@
 const mysql = require('mysql2/promise')
+
+// import mysql from 'mysql2/promise';
+
+require('dotenv').config();
+
+// export default connect = async () => {
+//     return mysql.createConnection({
+//         host:process.env.MY_HOST,
+//         user: process.env.MY_USER,
+//         password: process.env.MY_PASSWORD,
+//         database: process.env.MY_DB,
+//     })
+// } 
+
 const connection = mysql.createConnection({
     host:process.env.MY_HOST,
     user: process.env.MY_USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB,     
+    password: process.env.MY_PASSWORD,
+    database: process.env.MY_DB,     
 })
+
 
 module.exports = connection;
